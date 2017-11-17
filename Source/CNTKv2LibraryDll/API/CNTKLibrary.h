153ed97621a352e0be5333909d4384951a6f95e5
KeDengMS <kedeng@microsoft.com>
2017-11-16 18:15:39 -0800

    Fix the excessive print of NcclComm::Initialized in Evaluator

--- Source/CNTKv2LibraryDll/API/CNTKLibrary.h
+++ Source/CNTKv2LibraryDll/API/CNTKLibrary.h
@@ -5161,6 +5161,14 @@ namespace CNTK
     protected:
         Evaluator(const FunctionPtr& evaluationFunction, const std::vector<ProgressWriterPtr>& progressWriters = {}, bool initializeCombined = true);
 
+        void SetCommunicator(DistributedCommunicatorPtr communicator)
+        {
+            if (m_communicator != nullptr)
+                LogicError("Communicator has already been initialized.");
+
+            m_communicator = communicator;
+        }
+
         // Helper functions.
         std::vector<Variable> GetCombinedEvalFunctionArgs() const;
         static size_t GetSampleCount(const Variable& var, const ValuePtr& value);
@@ -5187,6 +5195,7 @@ namespace CNTK
         const AccumulatorPtr m_aggregatedTestEvalCriterionValue;
         Variable             m_testSampleCountVar;
         FunctionPtr          m_combinedEvalFunction;
+        DistributedCommunicatorPtr m_communicator;
     };
 
     ///
